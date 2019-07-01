452f14dcbdd80e99fa745365cff5ab890ef374cc
mikedelong <mikedelong@outlook.com>
2019-07-01 08:56:29 -0400

    feature: timing and logging, more boilerplate

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import json
+from time import time
+
+# import pandas as pd
+# from gensim.models import Word2Vec
+# from nltk.tokenize import casual_tokenize
+# from sklearn.decomposition import PCA
+# from sklearn.manifold.t_sne import TSNE
+# from nltk.corpus import stopwords
+import logging
 
 if __name__ == '__main__':
+    time_start = time()
     with open('./settings.json') as settings_fp:
         settings = json.load(settings_fp)
         print(settings)
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
