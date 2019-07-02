2680d5d0cdb4dce67b723ffd96fb1a644e2b37ce
mikedelong <mikedelong@outlook.com>
2019-07-02 10:14:08 -0400

    feature: introduce Word2Vec

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import json
-# import pandas as pd
-# from gensim.models import Word2Vec
-# from nltk.tokenize import casual_tokenize
 # from sklearn.decomposition import PCA
 # from sklearn.manifold.t_sne import TSNE
 # from nltk.corpus import stopwords
 import logging
 from time import time
+
+# import pandas as pd
+from gensim.models import Word2Vec
+from nltk.tokenize import casual_tokenize
 
 if __name__ == '__main__':
     time_start = time()
@@ -23,3 +24,14 @@ if __name__ == '__main__':
     with open(input_file, 'r', encoding='utf-8') as input_fp:
         text = input_fp.readlines()
         print('our input data has {} lines.'.format(len(text)))
+
+    time_word2vec = time()
+
+    random_state_ = 1
+    word2vec_size_ = 500  # how big are the word-to-vec vectors?
+    word2vec_min_count_ = 10  # how many times does a word have to appear to be interesting?
+    word2vec_workers_ = 4  # how many threads will we use?
+    word2vec_model = Word2Vec(min_count=word2vec_min_count_, seed=random_state_, size=word2vec_size_,
+                              workers=word2vec_workers_)
+    training_data = [[word.lower() for word in casual_tokenize(item)] for item in text]
+    print('word2vec took {:5.2f}s'.format(time() - time_word2vec))
