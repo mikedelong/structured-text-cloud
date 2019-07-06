40ca97dd661cefc612d19b346ad2afee7d8d4815
mikedelong <mikedelong@outlook.com>
2019-07-06 16:30:02 -0400

    feature: reducing the size of the vector space again starts to get us some mildly interesting clusters

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     time_word2vec = time()
 
     random_state_ = 1
-    word2vec_size_ = 200  # how big are the word-to-vec vectors?
+    word2vec_size_ = 100  # how big are the word-to-vec vectors?
     word2vec_min_count_ = 9  # how many times does a word have to appear to be interesting?
     word2vec_workers_ = 4  # how many threads will we use?
     word2vec_compute_loss_ = True
