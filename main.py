cd44dc65b7c90c1a12a3f516261acd014f22bdbd
mikedelong <mikedelong@outlook.com>
2019-07-22 11:08:22 -0400

    refactor: make the choice to use t-SNE instead of Isomap a setting

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ if __name__ == '__main__':
         logging.warning('setting projection (t-SNE/Isomap) iteration count to default'.format(n_iter_))
     # todo make this a setting
     do_tsne = True
+    do_tsne = settings['do_tsne'] if 'do_tsne' in settings.keys() else False
     if input_file is None:
         print('input file not in settings. Quitting.')
         quit(1)
