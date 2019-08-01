49d73d197860516d335ee26fbab19ebedd76e269
mikedelong <mikedelong@outlook.com>
2019-08-01 07:28:39 -0400

    cleanup: no functional changes, just whitespace

--- display.py
+++ display.py
@@ -28,9 +28,7 @@ if __name__ == '__main__':
     if do_basic:
         fig = go.Figure(data=[go.Scatter(hoverinfo='none',
                                          marker=dict(line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
-                                                     opacity=0.8,
-                                                     size=6),
-                                         mode=mode_, text=data_df['word'],
+                                                     opacity=0.8, size=6), mode=mode_, text=data_df['word'],
                                          x=data_df['x'], y=data_df['y'])],
                         layout=go.Layout(margin=dict(l=0, t=0, r=0, b=0)))
         plot(fig, filename=output_file_name, auto_open=False)
