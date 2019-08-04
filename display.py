3c8f8a35069d5c48d26222d305e99d1a4ed21501
mikedelong <mikedelong@outlook.com>
2019-08-03 20:03:56 -0400

    feature: add the sine wave code we're going to try to cut to fit

--- display.py
+++ display.py
@@ -2,6 +2,7 @@ import json
 import logging
 from time import time
 
+import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 from plotly.offline import plot
@@ -34,7 +35,26 @@ if __name__ == '__main__':
                                          x=data_df['x'], y=data_df['y'])],
                         layout=go.Layout(margin=dict(l=0, t=0, r=0, b=0)))
     else:
-        fig = go.Figure()
+        length = len(list(np.arange(0, 5, 0.1)))
+        fig = go.Figure(data=[
+            # fig.add_trace(
+            go.Scatter(
+                visible=False if index != 10 else True,
+                line=dict(color="#00CED1", width=6),
+                name="nu = " + str(step),
+                x=np.arange(0, 10, 0.01),
+                y=np.sin(step * np.arange(0, 10, 0.01)))
+
+            for index, step in enumerate(np.arange(0, 5, 0.1))
+
+        ], layout=[dict(sliders=dict(
+            active=10,
+            currentvalue={"prefix": "Frequency: "},
+            pad={"t": 50},
+            steps=[dict(method='restyle', args=['visible', [False if i != j else True for j in range(length)]]) for i
+                   in range(length)]
+        )
+        )])
     plot(fig, filename=output_file_name, auto_open=False)
 
     print('total time: {:5.2f}s'.format(time() - time_start))
