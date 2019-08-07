f384e80bcb041b0dc3df37e9de4450bbe0a9fef7
mikedelong <mikedelong@outlook.com>
2019-08-07 08:52:30 -0400

    feature: build the figure starting with the list of traces, rather than an empty Figure instance

--- demo_plotly_slider.py
+++ demo_plotly_slider.py
@@ -6,28 +6,51 @@ from plotly.offline import plot
 
 if __name__ == '__main__':
     # Create figure
-    fig = Figure()
 
     # Add traces, one for each slider step
     length = len(list(np.arange(0, 5, 0.1)))
-    for index, step in enumerate(np.arange(0, 5, 0.1)):
-        fig.add_trace(Scatter(
-            visible=(index == 10),
-            line=dict(color='#00CED1', width=6),
-            name='nu = ' + str(step),
-            x=np.arange(0, 10, 0.01),
-            y=np.sin(step * np.arange(0, 10, 0.01))))
 
-    # Create and add slider
+    do_legacy = False
+    if do_legacy:
+        fig = Figure()
+        for index, step in enumerate(np.arange(0, 5, 0.1)):
+            fig.add_trace(Scatter(
+                visible=(index == 10),
+                line=dict(color='#00CED1', width=6),
+                name='nu = ' + str(step),
+                x=np.arange(0, 10, 0.01),
+                y=np.sin(step * np.arange(0, 10, 0.01))))
 
-    fig.update_layout(
-        sliders=[dict(
-            active=10,
-            currentvalue={'prefix': 'Frequency: '},
-            pad={'t': 50},
-            steps=[dict(method='restyle', args=['visible', [j == i for j in range(length)]]) for i in range(length)]
-        )]
-    )
+        # Create and add slider
+
+        fig.update_layout(
+            sliders=[dict(
+                active=10,
+                currentvalue={'prefix': 'Frequency: '},
+                pad={'t': 50},
+                steps=[dict(method='restyle', args=['visible', [j == i for j in range(length)]]) for i in range(length)]
+            )]
+        )
+    else:
+        traces = [
+            Scatter(
+                visible=(index == 10),
+                line=dict(color='#00CED1', width=6),
+                name='nu = ' + str(step),
+                x=np.arange(0, 10, 0.01),
+                y=np.sin(step * np.arange(0, 10, 0.01)))
+            for index, step in enumerate(np.arange(0, 5, 0.1))
+        ]
+
+        fig = Figure(data=traces)
+        fig.update_layout(
+            sliders=[dict(
+                active=10,
+                currentvalue={'prefix': 'Frequency: '},
+                pad={'t': 50},
+                steps=[dict(method='restyle', args=['visible', [j == i for j in range(length)]]) for i in range(length)]
+            )]
+        )
 
     # fig = go.Figure(data=[
     #     # fig.add_trace(
