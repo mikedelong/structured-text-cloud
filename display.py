6fbc4caeaabf0ed75c2523126c26059a6a2c3820
mikedelong <mikedelong@outlook.com>
2019-08-08 10:45:19 -0400

    feature: bring in the functioning sine wave code

--- display.py
+++ display.py
@@ -39,20 +39,20 @@ if __name__ == '__main__':
         length = len(list(np.arange(0, 5, 0.1)))
         fig = go.Figure(data=[
             go.Scatter(
-                visible=False if index != 10 else True,
-                marker=dict(line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
-                            opacity=0.8, size=6), mode=mode_, text=data_df['word'],
-                x=data_df['x'], y=data_df['y'])
+                visible=(index == 10),
+                line=dict(color='#00CED1', width=6),
+                name='nu = ' + str(step),
+                x=np.arange(0, 10, 0.01),
+                y=np.sin(step * np.arange(0, 10, 0.01)))
             for index, step in enumerate(np.arange(0, 5, 0.1))
-
-        ], layout=[dict(sliders=dict(
-            active=10,
-            currentvalue={'prefix': 'Frequency: '},
-            pad={'t': 50},
-            steps=[dict(method='restyle', args=['visible', [False if i != j else True for j in range(length)]]) for i
-                   in range(length)]
-        )
-        )])
+        ], layout=dict(
+            sliders=[dict(
+                active=10,
+                currentvalue={'prefix': 'Frequency: '},
+                pad={'t': 50},
+                steps=[dict(method='restyle', args=['visible', [j == i for j in range(length)]]) for i in range(length)]
+            )]
+        ))
     plot(fig, filename=output_file_name, auto_open=False)
 
     print('total time: {:5.2f}s'.format(time() - time_start))
