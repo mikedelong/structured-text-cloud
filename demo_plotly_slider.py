322bfa1e135395bc22434eacc4c92cc6ffdafa7b
mikedelong <mikedelong@outlook.com>
2019-08-08 10:43:32 -0400

    cleanup: remove some dead commented-out code

--- demo_plotly_slider.py
+++ demo_plotly_slider.py
@@ -49,25 +49,6 @@ if __name__ == '__main__':
             )]
         ))
 
-    # fig = go.Figure(data=[
-    #     # fig.add_trace(
-    #     go.Scatter(
-    #         visible=False if index != 10 else True,
-    #         line=dict(color='#00CED1', width=6),
-    #         name='nu = ' + str(step),
-    #         x=np.arange(0, 10, 0.01),
-    #         y=np.sin(step * np.arange(0, 10, 0.01)))
-    #
-    #     for index, step in enumerate(np.arange(0, 5, 0.1))
-    #
-    # ], layout=[dict(sliders=dict(
-    #     active=10,
-    #     currentvalue={'prefix': 'Frequency: '},
-    #     pad={'t': 50},
-    #     steps=[dict(method='restyle', args=['visible', [False if i != j else True for j in range(length)]]) for i
-    #            in range(length)]
-    # )
-    # )])
     output_file_name = './demo_plotly_slider.html'
     plot(fig, filename=output_file_name, auto_open=False)
 
