9f86fddb1009978bebdf69fdb810423b920fca17
mikedelong <mikedelong@outlook.com>
2019-08-08 10:42:02 -0400

    feature: we finally have everything into a single Figure() call, but the code needs some cleanup

--- demo_plotly_slider.py
+++ demo_plotly_slider.py
@@ -32,7 +32,17 @@ if __name__ == '__main__':
             )]
         )
     else:
-        traces = [
+        # traces = [
+        #     Scatter(
+        #         visible=(index == 10),
+        #         line=dict(color='#00CED1', width=6),
+        #         name='nu = ' + str(step),
+        #         x=np.arange(0, 10, 0.01),
+        #         y=np.sin(step * np.arange(0, 10, 0.01)))
+        #     for index, step in enumerate(np.arange(0, 5, 0.1))
+        # ]
+
+        fig = Figure(data=[
             Scatter(
                 visible=(index == 10),
                 line=dict(color='#00CED1', width=6),
@@ -41,8 +51,7 @@ if __name__ == '__main__':
                 y=np.sin(step * np.arange(0, 10, 0.01)))
             for index, step in enumerate(np.arange(0, 5, 0.1))
         ]
-
-        fig = Figure(data=traces, layout=dict(
+            , layout=dict(
             sliders=[dict(
                 active=10,
                 currentvalue={'prefix': 'Frequency: '},
