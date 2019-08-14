f4b82e94b4cdbc24d29c5a2088947a095bb39d79
mikedelong <mikedelong@outlook.com>
2019-08-14 10:13:37 -0400

    feature: instead of stepping up from frame to frame by a set amount, let's traverse the sorted list of unique counts

--- display.py
+++ display.py
@@ -35,16 +35,15 @@ if __name__ == '__main__':
                         layout=go.Layout(margin=dict(l=0, t=0, r=0, b=0)))
     else:
         # todo telescope the levels so we don't have a bunch that are essentially the same
-        ratio = 5
-        length = data_df['count'].max() // ratio
+        length = data_df['count'].nunique()
         fig = go.Figure(data=[go.Scatter(hoverinfo='none',
                                          marker=dict(line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
                                                      opacity=0.8, size=6), mode=mode_,
-                                         text=data_df[data_df['count'] > ratio * index]['word'],
-                                         x=data_df[data_df['count'] > ratio * index]['x'],
-                                         y=data_df[data_df['count'] > ratio * index]['y'],
+                                         text=data_df[data_df['count'] > index]['word'],
+                                         x=data_df[data_df['count'] > index]['x'],
+                                         y=data_df[data_df['count'] > index]['y'],
                                          name='level: {}'.format(index)
-                                         ) for index in range(1, length)],
+                                         ) for index in sorted(data_df['count'].unique().tolist())],
                         layout=dict(
                             sliders=[dict(
                                 active=min(10, length),
