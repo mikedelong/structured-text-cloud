50605110491e6e09cc68017034345435b6bed85e
mikedelong <mikedelong@outlook.com>
2019-08-20 08:47:24 -0400

    feature: rejigger the scatter plot steps according to a fixed number of quantiles to make display more uniform from case to case

--- display.py
+++ display.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from collections import Counter
 from time import time
 
 import pandas as pd
@@ -25,6 +26,7 @@ if __name__ == '__main__':
         logging.error('No output file specified. Quitting.')
         quit(1)
     mode_ = 'text'  # 'markers+text'
+    slice_limit = 10
     do_basic = False
     if do_basic:
         fig = go.Figure(data=[go.Scatter(hoverinfo='none',
@@ -34,23 +36,49 @@ if __name__ == '__main__':
                                          )],
                         layout=go.Layout(margin=dict(l=0, t=0, r=0, b=0)))
     else:
-        length = data_df['count'].nunique()
-        fig = go.Figure(data=[go.Scatter(hoverinfo='none',
-                                         marker=dict(line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
-                                                     opacity=0.8, size=6), mode=mode_,
-                                         text=data_df[data_df['count'] > index]['word'],
-                                         x=data_df[data_df['count'] > index]['x'],
-                                         y=data_df[data_df['count'] > index]['y'],
-                                         name='level: {}'.format(index)
-                                         ) for index in sorted(data_df['count'].unique().tolist())],
-                        layout=dict(
-                            sliders=[dict(
-                                active=min(10, length),
-                                pad={'t': 1},
-                                steps=[dict(method='restyle', args=['visible', [j == i for j in range(length)]]) for i
-                                       in range(length)]
-                            )]
-                        ))
+        slices = sorted(data_df['count'].unique().tolist())
+
+        # if we have more than ten slices refit to ten
+        if len(slices) > slice_limit:
+            total = sum(slices)
+            counts = Counter(data_df['count'].values.tolist())
+            data_df = data_df.sort_values(by=['count'], axis=0, ascending=True)
+            data_df['cumulative'] = data_df['count'].cumsum()
+            quantiles = [1.0 / float(slice_limit) * index - 1.0 / (2.0 * float(slice_limit)) for index in
+                         range(1, slice_limit + 1)]
+            pass
+        else:
+            quantiles = [float(index) / float(len(slices)) for index in range(1, len(slices))]
+        interpolation_ = 'lower'
+        fig = go.Figure(data=[go.Scatter(
+            # hoverinfo='none',
+            marker=dict(line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
+                        opacity=0.8, size=6), mode=mode_,
+            # text=data_df[data_df['count'] > index]['word'],
+            text=
+            data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile, interpolation=interpolation_)][
+                'word'],
+            # x=data_df[data_df['count'] > index]['x'],
+            x=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile, interpolation=interpolation_)][
+                'x'],
+            # y=data_df[data_df['count'] > index]['y'],
+            y=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile, interpolation=interpolation_)][
+                'y'],
+            # hovertext=data_df[data_df['count'] > index]['count'],
+            hovertext=
+            data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile, interpolation=interpolation_)][
+                'count'],
+            name='level: {}'.format(index)
+        ) for index, quantile in enumerate(quantiles)],
+            layout=dict(
+                sliders=[dict(
+                    active=min(5, len(quantiles)),
+                    pad={'t': 1},
+                    # steps=[dict(method='restyle', args=['visible', [j == i for j in range(len(slices))]]) for i  in range(len(slices))]
+                    steps=[dict(method='restyle', args=['visible', [j == i for j in range(len(quantiles))]]) for i in
+                           range(len(quantiles))]
+                )]
+            ))
     plot(fig, filename=output_file_name, auto_open=False)
 
     print('total time: {:5.2f}s'.format(time() - time_start))
