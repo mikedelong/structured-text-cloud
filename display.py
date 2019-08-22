d965562ab4c9f932963d1446c0a8095000d90201
mikedelong <mikedelong@outlook.com>
2019-08-22 11:45:41 -0400

    cleanup: remove the basic code in favor of the slider version now that we have it working almost acceptably

--- display.py
+++ display.py
@@ -27,55 +27,46 @@ if __name__ == '__main__':
         quit(1)
     mode_ = 'text'  # 'markers+text'
     slice_limit = 10
-    do_basic = False
-    if do_basic:
-        fig = go.Figure(data=[go.Scatter(hoverinfo='none',
-                                         marker=dict(line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
-                                                     opacity=0.8, size=6), mode=mode_, text=data_df['word'],
-                                         x=data_df['x'], y=data_df['y']
-                                         )],
-                        layout=go.Layout(margin=dict(l=0, t=0, r=0, b=0)))
+    slices = sorted(data_df['count'].unique().tolist())
+
+    # if we have more than ten slices refit to ten
+    if len(slices) > slice_limit:
+        total = sum(slices)
+        counts = Counter(data_df['count'].values.tolist())
+        data_df = data_df.sort_values(by=['count'], axis=0, ascending=True)
+        data_df['cumulative'] = data_df['count'].cumsum()
+        quantiles = [1.0 / float(slice_limit) * index - 1.0 / (2.0 * float(slice_limit)) for index in
+                     range(1, slice_limit + 1)]
+        pass
     else:
-        slices = sorted(data_df['count'].unique().tolist())
+        quantiles = [float(index) / float(len(slices)) for index in range(1, len(slices))]
+    interpolation_ = 'lower'
 
-        # if we have more than ten slices refit to ten
-        if len(slices) > slice_limit:
-            total = sum(slices)
-            counts = Counter(data_df['count'].values.tolist())
-            data_df = data_df.sort_values(by=['count'], axis=0, ascending=True)
-            data_df['cumulative'] = data_df['count'].cumsum()
-            quantiles = [1.0 / float(slice_limit) * index - 1.0 / (2.0 * float(slice_limit)) for index in
-                         range(1, slice_limit + 1)]
-            pass
-        else:
-            quantiles = [float(index) / float(len(slices)) for index in range(1, len(slices))]
-        interpolation_ = 'lower'
+    fig = go.Figure(data=[go.Scatter(
+        marker=dict(line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
+                    opacity=0.8, size=6), mode=mode_,
+        text=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile,
+                                                                            interpolation=interpolation_)]['word'],
+        x=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile,
+                                                                         interpolation=interpolation_)]['x'],
+        y=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile,
+                                                                         interpolation=interpolation_)]['y'],
+        hovertext=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile,
+                                                                                 interpolation=interpolation_)][
+            'count'],
+        name='level: {}'.format(index),
 
-        fig = go.Figure(data=[go.Scatter(
-            marker=dict(line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
-                        opacity=0.8, size=6), mode=mode_,
-            text=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile,
-                                                                                interpolation=interpolation_)]['word'],
-            x=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile,
-                                                                             interpolation=interpolation_)]['x'],
-            y=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile,
-                                                                             interpolation=interpolation_)]['y'],
-            hovertext=data_df[data_df['cumulative'] > data_df['cumulative'].quantile(q=quantile,
-                                                                                     interpolation=interpolation_)][
-                'count'],
-            name='level: {}'.format(index),
-
-        ) for index, quantile in enumerate(quantiles)],
-            layout=dict(
-                sliders=[dict(
-                    active=len(quantiles) // 2,
-                    pad={'t': 1},
-                    steps=[dict(method='restyle', args=['visible', [j == i for j in range(len(quantiles))]]) for i in
-                           range(len(quantiles))]
-                )],
-                xaxis=dict(visible=False, range=[-100, 100]),
-                yaxis=dict(visible=False, range=[-100, 100]),
-            ))
+    ) for index, quantile in enumerate(quantiles)],
+        layout=dict(
+            sliders=[dict(
+                active=len(quantiles) // 2,
+                pad={'t': 1},
+                steps=[dict(method='restyle', args=['visible', [j == i for j in range(len(quantiles))]]) for i in
+                       range(len(quantiles))]
+            )],
+            xaxis=dict(visible=False, range=[-100, 100]),
+            yaxis=dict(visible=False, range=[-100, 100]),
+        ))
     plot(fig, filename=output_file_name, auto_open=False)
 
     logging.info('total time: {:5.2f}s'.format(time() - time_start))
