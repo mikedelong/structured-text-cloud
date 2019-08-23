dc5bfbe8567366c1c019fa83833ba433b119f7b0
mikedelong <mikedelong@outlook.com>
2019-08-23 10:38:22 -0400

    feature: initial demo of grabbing part of speech

--- demo_wiktionary.py
+++ demo_wiktionary.py
@@ -1,2 +1,7 @@
+from wiktionaryparser import WiktionaryParser
+
 if __name__ == '__main__':
-    pass
+    parser = WiktionaryParser()
+    result = parser.fetch('should')
+    for definition in result[0]['definitions']:
+        print(definition['partOfSpeech'])
