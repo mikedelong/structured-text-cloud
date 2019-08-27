6356962717ddbbb8383cfd83f5245cfc971b3487
mikedelong <mikedelong@outlook.com>
2019-08-27 15:06:39 -0400

    bug fix: let's disable the color data until we can use it

--- display.py
+++ display.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     interpolation_ = 'lower'
 
     data_df['part_of_speech'] = data_df['word'].apply(get_part_of_speech, args=(parser,))
-    data_df['color'] = data_df['part_of_speech'].map(part_of_speech_color_map)
+    # data_df['color'] = data_df['part_of_speech'].map(part_of_speech_color_map)
     stretch_factor = 1.05
     fig = go.Figure(data=[go.Scatter(
         marker=dict(line=dict(color='rgba(217, 217, 217, 0.14)', width=0.1),
