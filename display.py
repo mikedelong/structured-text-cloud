28ba7d3230c5f34c284bc9ad7444b9b096f265af
mikedelong <mikedelong@outlook.com>
2019-08-29 11:21:35 -0400

    feature: the first working version with varying color text labels just drops the rows where the color is missing from our map

--- display.py
+++ display.py
@@ -80,6 +80,7 @@ if __name__ == '__main__':
     # write the known parts of speech to a file before we proceed
     data_df[['word', 'part_of_speech']].to_csv('./data/part_of_speech.csv', index=True, header=True)
     data_df['color'] = data_df['part_of_speech'].map(part_of_speech_color_map)
+    data_df = data_df.dropna()
 
     stretch_factor = 1.05
     fig = go.Figure(data=[go.Scatter(
