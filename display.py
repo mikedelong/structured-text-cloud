d92f07918c28f06cf08ae3b0c66ba4d65653e776
mikedelong <mikedelong@outlook.com>
2019-09-07 09:28:20 -0400

    todo: added a TODO

--- display.py
+++ display.py
@@ -84,6 +84,7 @@ if __name__ == '__main__':
     colormap = cm.get_cmap('jet')
     if which_color_map == 'cumsum':
         # use the cumsum of the value counts to assign a gray by hex string
+        # todo get this working with the colormap
         part_of_speech_color_map = data_df['part_of_speech'].value_counts(normalize=True).cumsum(
         ).apply(lambda x: '#{:02x}{:02x}{:02x}'.format(int(256 * x - 1), int(256 * x - 1), int(256 * x - 1))).to_dict()
     elif which_color_map == 'uniform':
