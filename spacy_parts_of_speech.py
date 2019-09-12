9786fe5a626ac0ddee974675880a50fac1275a05
mikedelong <mikedelong@outlook.com>
2019-09-12 11:42:13 -0400

    feature: we should always be logging rather than printing if we want to keep all the messages in order

--- spacy_parts_of_speech.py
+++ spacy_parts_of_speech.py
@@ -16,13 +16,13 @@ if __name__ == '__main__':
     start_line = settings['text_start_line'] if 'text_start_line' in settings.keys() else 0
     stop_line = settings['text_stop_line'] if 'text_stop_line' in settings.keys() else -1
     if input_file is None:
-        print('input file not in settings. Quitting.')
+        logging.warning('input file not in settings. Quitting.')
         quit(1)
 
     input_encoding = settings['input_encoding'] if 'input_encoding' in settings.keys() else 'utf-8'
     with open(input_file, 'r', encoding=input_encoding) as input_fp:
         text = input_fp.readlines()
-        print('our input data has {} lines.'.format(len(text)))
+        logging.info('our input data has {} lines.'.format(len(text)))
 
     text = ' '.join(text[start_line: stop_line])  # exclude everything outside our window of interest
 
@@ -32,4 +32,4 @@ if __name__ == '__main__':
     result = parser(text=text)
     logging.info('parsing complete')
 
-    print('total time: {:5.2f}s'.format(time() - time_start))
+    logging.info('total time: {:5.2f}s'.format(time() - time_start))
